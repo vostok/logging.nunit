68e9b43a62c838ea4f462a6b612fe7e85abb0581
kungurtsev <kungurtsev@skbkontur.ru>
2022-08-08 13:14:50 +0500

    add settings class

---
+++ Vostok.Logging.NUnit/NUnitLogSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework.Internal;
+using Vostok.Logging.Formatting;
+
+namespace Vostok.Logging.NUnit;
+
+public class NUnitLogSettings
+{
+    public Func<TestExecutionContext>? ContextProvider { get; set; }
+
+    public OutputTemplate OutputTemplate { get; set; } = OutputTemplate.Default;
+}
